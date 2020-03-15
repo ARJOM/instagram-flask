cbb13bcde4ab6965ed7116336d1ad9eae91f88c9
Ricart <ricarteusuo11@gmail.com>
2020-03-15 11:00:54 -0300

    add database connection and sql script

--- app/views.py
+++ app/views.py
@@ -1,5 +1,22 @@
+from flask import g
+
+import psycopg2.extras
+
 from app import app
 
+
+# ~~~~
+@app.before_request
+def before_request():
+    g.db = psycopg2.connect('dbname=instagram user=flask password=flask host=127.0.0.1')
+
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+
+# ~~~~
 
 @app.route('/')
 def hello_world():
---
+++ db/instagram.sql
@@ -0,0 +1,43 @@
+CREATE TABLE users (
+	username varchar(80),
+	name varchar(80),
+	email varchar(80) UNIQUE,
+	password varchar(20) NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY (username)
+);
+
+CREATE TABLE photos (
+	id serial,
+	published_date date NOT NULL,
+	description varchar(2200),
+	username varchar(80),
+	way text NOT NULL,
+	CONSTRAINT photo_pk PRIMARY KEY (id),
+	CONSTRAINT user_fk FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE followers (
+	followed varchar(80),
+	follower varchar(80),
+	CONSTRAINT follower_fk FOREIGN KEY (follower) REFERENCES users(username),
+	CONSTRAINT followed_fk FOREIGN KEY (followed) REFERENCES users(username)
+);
+
+CREATE TABLE comments (
+	id serial,
+	created_date date NOT NULL,
+	content varchar(2200) NOT NULL,
+	photo integer NOT NULL,
+	username VARCHAR(80) NOT NULL,
+	CONSTRAINT comment_pk PRIMARY KEY (id),
+	CONSTRAINT photo_fk FOREIGN KEY (photo) REFERENCES photos(id),
+	CONSTRAINT user_fk FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE likes (
+	username varchar(80),
+	photo integer,
+	CONSTRAINT like_pk PRIMARY KEY (username, photo),
+	CONSTRAINT user_fk FOREIGN KEY (username) REFERENCES users(username),
+	CONSTRAINT photo_fk FOREIGN KEY (photo) REFERENCES photos(id)
+);
